7a9b4b2f00727e99155a0395ad131ac4801f1ff5
mheepandaaa <72680344+mheepandaaa@users.noreply.github.com>
2020-11-27 15:44:39 +0700

    Update update.php

--- update.php
+++ update.php
@@ -7,7 +7,6 @@ $Price=$_POST['Price'];
 $Discount=$_POST['Discount'];
 $discounted = ($Price * $Discount)/100;
 $Total = $Price - $discounted;
-$Total=$_POST['Total'];
 $sql="UPDATE Information SET Product='$Product', Price='$Price',Discount='$Discount',Total='$Total' WHERE ID='$id'";
 if (mysqli_query($conn, $sql)) {
     header("Location: show.php");

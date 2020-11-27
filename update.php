0cd22fd6ad3f43670ae4aa8070499f03a4f3126a
mheepandaaa <72680344+mheepandaaa@users.noreply.github.com>
2020-11-27 15:31:49 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,10 +2,11 @@
 $id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itflabtest084.mysql.database.azure.com', 'thanee@itflabtest084', 'Yongyong0', 'itflabtest', 3306);
-$Product=$_POST['Product'];
 $Price=$_POST['Price'];
 $Discount=$_POST['Discount'];
 $Total=$_POST['Total'];
+$discounted = ($Price * $Discount)/100;
+$Total = $Price - $discounted;
 $sql="UPDATE Information SET Price='$Price',Discount='$Discount',Total='$Total' WHERE ID='$id'";
 if (mysqli_query($conn, $sql)) {
     header("Location: show.php");
